7ec93c63f4af5ce911569a312046eece64e56659
Lerking <33354709+Lerking@users.noreply.github.com>
2022-07-20 12:42:14 +0200

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,16 @@
+EXERCISES = [
+    {'name': 'Push-ups', 'area': ['biceps', 'triceps']},
+    {'name': 'Squats', 'area': ['thigh', 'calf']},
+    {'name': 'Leg-lifts', 'area': ['thigh', 'abdomen']},
+    {'name': 'Lunges', 'area': ['thigh', 'calf']},
+    {'name': 'Plank', 'area': ['abdomen']},
+    {'name': 'Triceps dips', 'area': ['triceps']},
+    {'name': 'Crunches', 'area': ['abdomen']},
+    {'name': 'One-leg squats', 'area': ['thigh', 'calf']},
+    {'name': 'Single leg squats', 'area': ['thigh', 'calf']},
+    {'name': 'Calf raises', 'area': ['calf']},
+    {'name': 'Seated dips', 'area': ['triceps']},
+    {'name': 'Jumping jacks', 'area': ['full body']},
+    {'name': 'Side plank x2', 'area': ['abdomen', 'side hips', 'back hips']},
+    {'name': 'Arm circles', 'area': ['shoulders', 'biceps', 'triceps']},
+]
---
+++ tabata_randomizer.py
@@ -0,0 +1,61 @@
+from exercises import EXERCISES
+from random import shuffle
+
+class TabataRandomizer():
+    def __init__(self, number_of_sets: int = 8, number_of_reps: int = 4,
+                rest_time: int = 15, work_time: int = 30):
+        self.number_of_sets = number_of_sets
+        self.number_of_reps = number_of_reps
+        self.rest_time = rest_time
+        self.work_time = work_time
+        self.interval_time = self.rest_time + self.work_time
+        self.exercises = EXERCISES
+
+    def randomize(self):
+        exercises = self.exercises
+        shuffle(exercises)
+        for index, exercise in enumerate(exercises):
+            if exercise['name'] == 'Side plank x2':
+                if index <= 6:
+                    return exercises[:self.number_of_sets - 1]
+                elif index == 7:
+                    return exercises[:self.number_of_sets - 1:1]
+        return exercises[:self.number_of_sets]
+
+    def get_exercises(self):
+        return self.exercises
+
+    def get_training_plan(self):
+        training_plan = []
+        for exercise in self.randomize():
+            training_plan.append(exercise['name'])
+        return training_plan
+
+    def get_training_time(self):
+        return self.interval_time * self.number_of_sets * self.number_of_reps
+
+if __name__ == '__main__':
+    TE = TabataRandomizer()
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    print(TE.get_training_plan())
+    print(f'Combined training time: {TE.get_training_time() / 60} minutes')
+    pass
