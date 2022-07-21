65334f04fd05284fc4bb86bdd015665b57ebbcff
Lerking <33354709+Lerking@users.noreply.github.com>
2022-07-21 12:46:41 +0200

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -6,11 +6,13 @@ EXERCISES = [
     {'name': 'Plank', 'area': ['abdomen']},
     {'name': 'Triceps dips', 'area': ['triceps']},
     {'name': 'Crunches', 'area': ['abdomen']},
-    {'name': 'One-leg squats', 'area': ['thigh', 'calf']},
-    {'name': 'Single leg squats', 'area': ['thigh', 'calf']},
+    {'name': 'One-leg squats x2', 'area': ['thigh', 'calf']},
+    {'name': 'Single leg squats x2', 'area': ['thigh', 'calf']},
     {'name': 'Calf raises', 'area': ['calf']},
     {'name': 'Seated dips', 'area': ['triceps']},
     {'name': 'Jumping jacks', 'area': ['full body']},
     {'name': 'Side plank x2', 'area': ['abdomen', 'side hips', 'back hips']},
     {'name': 'Arm circles', 'area': ['shoulders', 'biceps', 'triceps']},
 ]
+
+DOUBLE_EXERCISES = ['One-leg squats x2', 'Single leg squats x2', 'Side plank x2']
